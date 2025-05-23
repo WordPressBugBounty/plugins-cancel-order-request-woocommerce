abeee7ff59788dd5369b71ddd36d76c8ea11b9f2
Sean Murphy <sean@iamseanmurphy.com>
2025-05-23 12:19:31 +0000

    1.3.4.2

--- cancel-order-request-woocommerce/cancel-order-request-woocommerce.php
+++ cancel-order-request-woocommerce/cancel-order-request-woocommerce.php
@@ -9,21 +9,21 @@
  * that starts the plugin.
  *
  * @link              piwebsolution.com
- * @since             1.3.4.1
+ * @since             1.3.4.2
  * @package           Cancel_Order_Request_Woocommerce
  *
  * @wordpress-plugin
  * Plugin Name:       Cancel order request for WooCommerce
  * Plugin URI:        piwebsolution.com/cancel-order-request-woocommerce
  * Description:       Gives option to replace Cancel order button with Cancel order request button, so user can send cancellation request
- * Version:           1.3.4.1
+ * Version:           1.3.4.2
  * Author:            PI Websolution
  * Author URI:        piwebsolution.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       cancel-order-request-woocommerce
  * Domain Path:       /languages
- * WC tested up to: 9.7.2
+ * WC tested up to: 9.8.0
  */
 
 // If this file is called directly, abort.
@@ -60,10 +60,10 @@ if(!is_plugin_active( 'woocommerce/woocommerce.php')){
 
 /**
  * Currently plugin version.
- * Start at version 1.3.4.1 and use SemVer - https://semver.org
+ * Start at version 1.3.4.2 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'CANCEL_ORDER_REQUEST_WOOCOMMERCE_VERSION', '1.3.4.1' );
+define( 'CANCEL_ORDER_REQUEST_WOOCOMMERCE_VERSION', '1.3.4.2' );
 
 define('PISOL_CORW_BASE_DIR', __DIR__);
 define('PISOL_CORW_PRICE', '$19');
@@ -83,6 +83,7 @@ add_action( 'before_woocommerce_init', function() {
  * This action is documented in includes/class-cancel-order-request-woocommerce-activator.php
  */
 function activate_cancel_order_request_woocommerce() {
+    add_option('pi_corw_do_activation_redirect', true);
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-cancel-order-request-woocommerce-activator.php';
 	Cancel_Order_Request_Woocommerce_Activator::activate();
 }
@@ -116,10 +117,20 @@ add_action( 'plugin_action_links_' . plugin_basename( __FILE__ ),  'pisol_cancel
 function pisol_cancel_order_request_plugin_link( $links ) {
     $links = array_merge( array(
         '<a href="' . esc_url( admin_url( '/admin.php?page=pisol-cancel-order-request' ) ) . '">' . __( 'Settings', 'cancel-order-request-woocommerce' ) . '</a>',
-        '<a style="color:#0a9a3e; font-weight:bold;" target="_blank" href="https://wordpress.org/support/plugin/cancel-order-request-woocommerce/reviews/#bbp_topic_content">' . __( 'Send suggestions to improve','cancel-order-request-woocommerce' ) . '</a>', '<a style="color:#FF0000; font-weight:bold;" target="_blank" href="'.esc_url(PISOL_CORW_BUY_URL).'">' . __( 'Get PRO','cancel-order-request-woocommerce' ) . '</a>'
+        '<a style="color:#0a9a3e; font-weight:bold;" target="_blank" href="https://wordpress.org/support/plugin/cancel-order-request-woocommerce/reviews/?filter=5#wp-bbp_topic_content-wrap">' . __( 'Send suggestions to improve','cancel-order-request-woocommerce' ) . '</a>', '<a style="color:#FF0000; font-weight:bold;" target="_blank" href="'.esc_url(PISOL_CORW_BUY_URL).'">' . __( 'Get PRO','cancel-order-request-woocommerce' ) . '</a>'
     ), $links );
     return $links;
 }
+
+add_action('admin_init', function (){
+    if (get_option('pi_corw_do_activation_redirect', false)) {
+        delete_option('pi_corw_do_activation_redirect');
+        if(!isset($_GET['activate-multi']))
+        {
+            wp_redirect("admin.php?page=pisol-cancel-order-request");
+        }
+    }
+});
 
 /**
  * Begins execution of the plugin.
@@ -128,7 +139,7 @@ function pisol_cancel_order_request_plugin_link( $links ) {
  * then kicking off the plugin from this point in the file does
  * not affect the page life cycle.
  *
- * @since    1.3.4.1
+ * @since    1.3.4.2
  */
 function run_cancel_order_request_woocommerce() {
 
