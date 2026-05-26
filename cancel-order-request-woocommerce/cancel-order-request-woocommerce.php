7f54cc19d666ae5fd4998b77791a1ea925361eef
Sean Murphy <sean@iamseanmurphy.com>
2026-05-26 13:24:19 +0000

    1.3.4.31

--- cancel-order-request-woocommerce/cancel-order-request-woocommerce.php
+++ cancel-order-request-woocommerce/cancel-order-request-woocommerce.php
@@ -9,14 +9,14 @@
  * that starts the plugin.
  *
  * @link              piwebsolution.com
- * @since             1.3.4.30
+ * @since             1.3.4.31
  * @package           Cancel_Order_Request_Woocommerce
  *
  * @wordpress-plugin
  * Plugin Name:       PiWeb Cancel order / Refund request for WooCommerce
  * Plugin URI:        https://www.piwebsolution.com/product/cancel-order-request-repeat-order-re-order-for-woocommerce/
  * Description:       Gives option to replace Cancel order button with Cancel order request button, so user can send cancellation request
- * Version:           1.3.4.30
+ * Version:           1.3.4.31
  * Author:            PI Websolution
  * Author URI:        https://piwebsolution.com
  * License:           GPL-2.0+
@@ -58,10 +58,10 @@ if(!is_plugin_active( 'woocommerce/woocommerce.php')){
 
 /**
  * Currently plugin version.
- * Start at version 1.3.4.30 and use SemVer - https://semver.org
+ * Start at version 1.3.4.31 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'CANCEL_ORDER_REQUEST_WOOCOMMERCE_VERSION', '1.3.4.30' );
+define( 'CANCEL_ORDER_REQUEST_WOOCOMMERCE_VERSION', '1.3.4.31' );
 
 define('PISOL_CORW_BASE_DIR', __DIR__);
 define('PISOL_CORW_PRICE', '$1.5 / month');
@@ -138,7 +138,7 @@ add_action('admin_init', function (){
  * then kicking off the plugin from this point in the file does
  * not affect the page life cycle.
  *
- * @since    1.3.4.30
+ * @since    1.3.4.31
  */
 function run_cancel_order_request_woocommerce() {
 
